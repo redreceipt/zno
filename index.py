c39bb40b105e1d0bd9caa051eebda79df8f874b5
Michael Neeley <micneeley14@gmail.com>
2019-09-21 12:14:30 -0400

    added logging

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, escape, request
 
 from zno import getInfo
 
@@ -7,5 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    info = getInfo("braveheart")
-    return info
+    return "search page"
+
+
+@app.route('/title/<title>')
+def title(title, method=["GET", "POST"]):
+    app.logger.debug(f"{request.url}")
+    if request.method == 'GET':
+        return getInfo(escape(title))
+    else:
+        return getInfo(request.form["search"])
+
+
+@app.route('/api/<title>')
+def api(title):
+    app.logger.debug(f"{request.url}")
+    return getInfo(escape(title))
+
+
+@app.errorhandler(404)
+def notFound(error):
+    app.logger.error(f"404 error: {error}")
+    return "page not found", 404
